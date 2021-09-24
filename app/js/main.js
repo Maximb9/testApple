08875450dab79534d734cc61611f48fc1a54d554
Maxim <bmw_077@mail.ru>
2021-09-24 16:04:22 +0300

    error

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+// let cone = document.querySelector('.choice__card');
+// let ctwo = document.querySelector('.image');
+
+// cone.onclick = function() {
+//     cone.classList.add("active");
+// }
+
+// ctwo.onclick = function() {
+//     ctwo.classList.add("active");
+// }
+
+// if (cone !== 'active') {
+//     cone.classList.add("active"); 
+// } else {
+//     cone.classList.remove("active");
+// }
+
+var userLang = navigator.language || navigator.userLanguage; 
+console.log(userLang);
